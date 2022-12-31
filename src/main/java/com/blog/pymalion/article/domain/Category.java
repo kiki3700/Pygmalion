f5603df7d1c93a9d0b4a0eec7a0cd1affc07dcfd
kiki3700 <5575492q@gmail.com>
2022-12-31 08:23:48 +0000

    working detail - article domain model.

--- src/main/java/com/blog/pymalion/article/domain/Category.java
+++ src/main/java/com/blog/pymalion/article/domain/Category.java
@@ -1,7 +1,12 @@
 package com.blog.pymalion.article.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
 public class Category {
     private UUID id;
     private String name;
--- src/main/java/com/blog/pymalion/article/domain/Comment.java
+++ src/main/java/com/blog/pymalion/article/domain/Comment.java
@@ -1,10 +1,14 @@
 package com.blog.pymalion.article.domain;
 
 import com.blog.pymalion.member.domain.Member;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 import java.util.UUID;
 
+@Getter
+@Setter
 public class Comment {
 
     private UUID commentId;
