fea13d386f97b56a38409e183140c2aef8abace3
kiki3700 <5575492q@gmail.com>
2023-01-26 12:43:56 +0000

    working detail - add transactional annotation.

--- src/main/java/com/blog/pymalion/member/application/MemberApplicationService.java
+++ src/main/java/com/blog/pymalion/member/application/MemberApplicationService.java
@@ -5,6 +5,7 @@ import com.blog.pymalion.member.domain.Member;
 import com.blog.pymalion.member.domain.MemberService;
 import com.blog.pymalion.member.domain.exception.NotRegisterException;
 import com.blog.pymalion.member.domain.exception.WrongPasswordException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +14,16 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class MemberApplicationService {
+
     private final MemberService memberService;
 
+    @Transactional
     public MemberAccount signUp(String email, String nickName, String password, Authority authority) {
         Member newMember = memberService.sighUp(email, nickName, password);
         return MemberAccount.of(newMember);
     }
 
+    @Transactional
     public MemberAccount signIn(String email, String password) throws NotRegisterException, WrongPasswordException {
         Member member = memberService.signIn(email, password);
         return MemberAccount.of(member);
